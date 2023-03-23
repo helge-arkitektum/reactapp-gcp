d81e21c546f34dc685d99d21c00b266a58009639
HelgeArkitektum <helge@arkitektum.no>
2023-03-23 11:22:01 +0100

    Endret tekst på knapp

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <div>
-        <button onClick={getProductsData1}>Get product 1</button>
+        <button onClick={getProductsData1}>Ikke i bruk</button>
       </div>
       <div>
         <button onClick={getProductsData2}>Get product - vis</button>
