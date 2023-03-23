860ac54ff38837533d4977956abd0565f50594e9
HelgeArkitektum <helge@arkitektum.no>
2023-03-23 14:39:27 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <div>
-        <button onClick={getProductsData1}>Ikke i bruk - boffevoff</button>
+        <button onClick={getProductsData1}>Ikke i bruk - boffevoff WOW</button>
       </div>
       <div>
         <button onClick={getProductsData2}>Get product - vis</button>
