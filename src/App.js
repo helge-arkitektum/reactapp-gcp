605ebc560a9c104a1145f39afd2451055b0c2481
HelgeArkitektum <helge@arkitektum.no>
2023-03-23 15:57:58 +0100

    Whaaat

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <div>
-        <button onClick={getProductsData1}>Ikke i bruk - boffevoff WOW</button>
+        <button onClick={getProductsData1}>Ikke i bruk - WHAAAAAT</button>
       </div>
       <div>
         <button onClick={getProductsData2}>Get product - vis</button>
