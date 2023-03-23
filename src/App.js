dd3faad8c627ad4c6ea58d450d04a9c3a0109f59
HelgeArkitektum <helge@arkitektum.no>
2023-03-23 12:29:57 +0100

    Another fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <div>
-        <button onClick={getProductsData1}>Ikke i bruk</button>
+        <button onClick={getProductsData1}>Ikke i bruk - voffvoff</button>
       </div>
       <div>
         <button onClick={getProductsData2}>Get product - vis</button>
